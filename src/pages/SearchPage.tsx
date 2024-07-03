2089bd9caf435cef33d20227eebbbbaedd1e6fca
adhityaizza <adhityaizza@students.amikom.ac.id>
2024-07-03 00:00:16 +0000

    fix searh page

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonThumbnail, IonList } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './SearchPage.css';
 
 const SearchPage: React.FC = () => {
+  const data = [
+    {
+      title: "koasong",
+      description: "namanya juga koasong",
+      image: "https://ionicframework.com/docs/img/demos/card-media.png"
+    },
+    {
+      title: "kek",
+      description: "enak pas di mam di ultah",
+      image: "https://ionicframework.com/docs/img/demos/card-media.png"
+    },
+    {
+      title: "pasutri",
+      description: "ini impian kalean kan sama si dia",
+      image: "https://ionicframework.com/docs/img/demos/card-media.png"
+    },
+    {
+      title: "kronologi",
+      description: "pokok enak bet dah",
+      image: "https://ionicframework.com/docs/img/demos/card-media.png"
+    },
+    {
+      title: "name",
+      description: "description",
+      image: "https://ionicframework.com/docs/img/demos/card-media.png"
+    },
+    {
+      title: "name",
+      description: "description",
+      image: "https://ionicframework.com/docs/img/demos/card-media.png"
+    },
+  ]
   return (
     <IonPage>
       <IonHeader>
@@ -11,50 +43,37 @@ const SearchPage: React.FC = () => {
           <IonTitle>Tab 3</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+      <IonContent fullscreen className="ion-padding">
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Account</IonTitle>
           </IonToolbar>
         </IonHeader>
+        
+        <h2>hasil dari pencarian kamu</h2>
 
-        <ion-card>
-          <ion-card-header>
-            <ion-card-title>Card Title</ion-card-title>
-            <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
-          </ion-card-header>
-          <ion-card-content>
-            <ion-list>
-              <ion-item>
-                <ion-thumbnail slot="start">
-                  <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
-                </ion-thumbnail>
-                <ion-label>Item</ion-label>
-              </ion-item>
-
-              <ion-item>
-                <ion-thumbnail slot="start">
-                  <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
-                </ion-thumbnail>
-                <ion-label>Item</ion-label>
-              </ion-item>
-
-              <ion-item>
-                <ion-thumbnail slot="start">
-                  <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
-                </ion-thumbnail>
-                <ion-label>Item</ion-label>
-              </ion-item>
-
-              <ion-item lines="none">
-                <ion-thumbnail slot="start">
-                  <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
-                </ion-thumbnail>
-                <ion-label>Item</ion-label>
-              </ion-item>
-            </ion-list>
-          </ion-card-content>
-        </ion-card>
+        {data.map(item => {
+          return (
+            <IonCard>
+            <div className='search-card'> 
+                <img 
+                    src={item.image} 
+                    alt={item.title}
+                />
+                <div>
+                    <IonCardHeader>
+                        <IonCardTitle>{item.title}</IonCardTitle>
+                        <IonCardSubtitle>{item.title}</IonCardSubtitle> 
+                    </IonCardHeader>
+                    <IonCardContent>
+                        <p>Price: {item.title}</p>
+                        <p>Rating: 5</p> 
+                    </IonCardContent>
+                </div>
+            </div>
+        </IonCard>
+          )
+        })}
 
       </IonContent>
     </IonPage>
